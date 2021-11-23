4cd6566b009b9e3754983c25c4ab55eb178ef1b1
johnoh <johnoh@usc.edu>
2021-11-23 03:06:45 -0500

    render notes logic

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -43,4 +43,19 @@ const saveNote = (note) =>
   });
 
 
+const renderActiveNote = () => {
+  hide(saveNoteBtn);
+
+  if (activeNote.id) {
+    noteTitle.setAttribute('readonly', true);
+    noteText.setAttribute('readonly', true);
+    noteTitle.value = activeNote.title;
+    noteText.value = activeNote.text;
+  } else {
+    noteTitle.removeAttribute('readonly');
+    noteText.removeAttribute('readonly');
+    noteTitle.value = '';
+    noteText.value = '';
+  }
+};
 
